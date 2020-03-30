02df404ffca432eaf0c99f70acecd27085c3e7da
ry99 <ryan@ostrich-emulators.com>
2020-03-30 16:47:48 -0400

    cell formatting

--- docker/frontend/PreVentDataCenter/src/main/java/com/ostrichemulators/prevent/PrimaryController.java
+++ docker/frontend/PreVentDataCenter/src/main/java/com/ostrichemulators/prevent/PrimaryController.java
@@ -2,14 +2,22 @@ package com.ostrichemulators.prevent;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -21,17 +29,18 @@ public class PrimaryController implements Initializable {
 	private TableView<WorkItem> table;
 
 	@FXML
-	private TableColumn<?, ?> statuscol;
+	private TableColumn<WorkItem, ?> statuscol;
 
 	@FXML
-	private TableColumn<?, ?> filecol;
+	private TableColumn<WorkItem, Path> filecol;
 
 	@FXML
-	private TableColumn<?, ?> startedcol;
+	private TableColumn<WorkItem, LocalDateTime> startedcol;
 
 	@FXML
-	private TableColumn<?, ?> endedcol;
+	private TableColumn<WorkItem, LocalDateTime> endedcol;
 
+	//private final List<WorkItem> items = new ArrayList<>();
 	@Override
 	public void initialize( URL url, ResourceBundle rb ) {
 		double sum = 0d;
@@ -45,6 +54,23 @@ public class PrimaryController implements Initializable {
 			cols[i].prefWidthProperty().bind(
 					table.widthProperty().multiply( pct ) );
 		}
+
+		ObservableList<WorkItem> titems = table.getItems();
+		Random r = new Random();
+		for ( int i = 0; i < 30; i++ ) {
+			WorkItem wi = new WorkItem( Paths.get( "c:\\", "more", Integer.toString( i ) ),
+					Integer.toHexString( r.nextInt() ),
+					LocalDateTime.now(), LocalDateTime.now() );
+			titems.add( wi );
+		}
+
+		filecol.setCellValueFactory( new PropertyValueFactory<>( "dir" ) );
+
+		startedcol.setCellValueFactory( new PropertyValueFactory<>( "started" ) );
+		startedcol.setCellFactory( column -> new LocalDateTableCell() );
+
+		endedcol.setCellValueFactory( new PropertyValueFactory<>( "finished" ) );
+		endedcol.setCellFactory( column -> new LocalDateTableCell() );
 	}
 
 	@FXML
@@ -62,5 +88,19 @@ public class PrimaryController implements Initializable {
 		stage.setTitle( "Create New Worklist Item" );
 		stage.setScene( new Scene( root1 ) );
 		stage.show();
+	}
+
+	private static class LocalDateTableCell extends TableCell<WorkItem, LocalDateTime> {
+
+		@Override
+		protected void updateItem( LocalDateTime item, boolean empty ) {
+			super.updateItem( item, empty );
+			if ( empty || null == item ) {
+				setText( "empty" );
+			}
+			else {
+				setText( item.format( DateTimeFormatter.ISO_DATE_TIME ) );
+			}
+		}
 	}
 }
--- docker/frontend/PreVentDataCenter/src/main/java/com/ostrichemulators/prevent/WorkItem.java
+++ docker/frontend/PreVentDataCenter/src/main/java/com/ostrichemulators/prevent/WorkItem.java
@@ -3,9 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.ostrichemulators.prevent;
-
 
 import java.nio.file.Path;
 import java.time.LocalDateTime;
@@ -17,9 +15,57 @@ import org.slf4j.LoggerFactory;
  * @author ryan
  */
 public class WorkItem {
+
 	private static final Logger LOG = LoggerFactory.getLogger( WorkItem.class );
 	private Path dir;
 	private String containerId;
 	private LocalDateTime started;
 	private LocalDateTime finished;
+
+	public WorkItem() {
+	}
+
+	public WorkItem( Path dir ) {
+		this.dir = dir;
+	}
+
+	public WorkItem( Path dir, String containerId, LocalDateTime started, LocalDateTime finished ) {
+		this.dir = dir;
+		this.containerId = containerId;
+		this.started = started;
+		this.finished = finished;
+	}
+
+	public Path getDir() {
+		return dir;
+	}
+
+	public void setDir( Path dir ) {
+		this.dir = dir;
+	}
+
+	public String getContainerId() {
+		return containerId;
+	}
+
+	public void setContainerId( String containerId ) {
+		this.containerId = containerId;
+	}
+
+	public LocalDateTime getStarted() {
+		return started;
+	}
+
+	public void setStarted( LocalDateTime started ) {
+		this.started = started;
+	}
+
+	public LocalDateTime getFinished() {
+		return finished;
+	}
+
+	public void setFinished( LocalDateTime finished ) {
+		this.finished = finished;
+	}
+
 }
