e7f27e07739467cef2743310790f30a8049866ff
ry99 <ryan@ostrich-emulators.com>
2020-04-28 10:04:15 -0400

    added script to build all docker images

---
+++ docker/build-all.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Syntax: $0 <version>"
+  exit 1
+fi
+
+VERSION=$1
+
+docker images --format '{{.Repository}}:{{.Tag}}'|grep prevent|sed -e 's/:<none>//g'|xargs docker rmi
+
+docker build -t ry99/prevent .
+docker build -t ry99/prevent-cnv --file Dockerfile-multiconvert .
+docker build -t ry99/prevent-tools --file Dockerfile-tools .
+
+IMAGEID=$(docker image ls|grep 'prevent ' | cut -c43-54)
+docker tag $IMAGEID "ry99/prevent:$VERSION" 
+docker tag $IMAGEID "ry99/prevent:$VERSION"
+
+IMAGEID=$(docker image ls|grep 'prevent-cnv' | cut -c43-54)
+docker tag $IMAGEID "ry99/prevent-cnv:$VERSION"
+docker tag $IMAGEID "ry99/prevent-cnv:$VERSION"
+
+IMAGEID=$(docker image ls|grep 'prevent-tool' | cut -c43-54)
+docker tag $IMAGEID "ry99/prevent-tools:$VERSION"
+docker tag $IMAGEID "ry99/prevent-tools:$VERSION"
+
+echo "Now run this:"
+echo "docker push ry99/prevent:latest"
+echo "docker push ry99/prevent:$VERSION"
+echo "docker push ry99/prevent-cnv:latest"
+echo "docker push ry99/prevent-cnv:$VERSION"
+echo "docker push ry99/prevent-tools:latest"
+echo "docker push ry99/prevent-tools:$VERSION"
+
