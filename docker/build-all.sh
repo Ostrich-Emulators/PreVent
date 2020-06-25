e2414125e70ac7504c6a845c5f4df778edcb40b5
ry99 <ryan@ostrich-emulators.com>
2020-06-25 12:36:20 -0400

    fixed docker hardcoded version

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -6,23 +6,29 @@ if [ $# -ne 1 ]; then
 fi
 
 VERSION=$1
+sed -e 's/XXVERSIONXX/'${VERSION}'/g' Dockerfile > .dockerfile-${VERSION}
+sed -e 's/XXVERSIONXX/'${VERSION}'/g' docker-base-setup.sh > .docker-base-setup-${VERSION}.sh
+sed -e 's/XXVERSIONXX/'${VERSION}'/g' Dockerfile-multiconvert > .dockerfile-multiconvert-${VERSION}
+sed -e 's/XXVERSIONXX/'${VERSION}'/g' Dockerfile-tools > .dockerfile-tools-${VERSION}
+
+chmod 755 .docker-base-setup-${VERSION}.sh
 
 docker images --format '{{.Repository}}:{{.Tag}}'|grep prevent|sed -e 's/:<none>//g'|xargs docker rmi
 
-docker build -t ry99/prevent .
-docker build -t ry99/prevent-cnv --file Dockerfile-multiconvert .
-docker build -t ry99/prevent-tools --file Dockerfile-tools .
+docker build -t ry99/prevent --file .dockerfile-${VERSION} .
+docker build -t ry99/prevent-cnv --file .dockerfile-multiconvert-${VERSION} .
+docker build -t ry99/prevent-tools --file .dockerfile-tools-${VERSION} .
 
 IMAGEID=$(docker image ls|grep 'prevent ' | cut -c43-54)
-docker tag $IMAGEID "ry99/prevent:$VERSION" 
+docker tag $IMAGEID "ry99/prevent:latest" 
 docker tag $IMAGEID "ry99/prevent:$VERSION"
 
 IMAGEID=$(docker image ls|grep 'prevent-cnv' | cut -c43-54)
-docker tag $IMAGEID "ry99/prevent-cnv:$VERSION"
+docker tag $IMAGEID "ry99/prevent-cnv:latest"
 docker tag $IMAGEID "ry99/prevent-cnv:$VERSION"
 
 IMAGEID=$(docker image ls|grep 'prevent-tool' | cut -c43-54)
-docker tag $IMAGEID "ry99/prevent-tools:$VERSION"
+docker tag $IMAGEID "ry99/prevent-tools:latest"
 docker tag $IMAGEID "ry99/prevent-tools:$VERSION"
 
 echo
