7cea7013bc5e601274fdc20b0e460f56b84fa3e3
ryan bobko <ryan@ostrich-emulators.com>
2021-08-10 13:46:49 -0400

    updated help

--- FormatConverter/main.cpp
+++ FormatConverter/main.cpp
@@ -66,7 +66,7 @@ void helpAndExit( char * progname, std::string msg = "" ) {
       << std::endl << "\t-w or --skip-waves"
       << std::endl << "\t-m or --tmpdir <directory>"
       << std::endl << "\t-R or --release (show release information and exit)"
-      << std::endl << "\tValid input formats: wfdb, hdf5, stpxml, stpge, stpp, cpcxml, stpjson, tdms, medi, dwc, zl, csv"
+      << std::endl << "\tValid input formats: wfdb, hdf5, stpxml, stpge, stpp, cpcxml, stpjson, tdms, medi, dwc, zl, csv, dwcx"
       << std::endl << "\tValid output formats: wfdb, hdf5, mat, csv, au"
       << std::endl << "\tthe --sqlite option will create/add metadata to a sqlite database"
       << std::endl << "\tthe --pattern option recognizes these format specifiers:"
